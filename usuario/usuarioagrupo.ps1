4545395672bb1e16a94e878fbddac48b8b727987
aluvaz5529 <91330510+aluvaz5529@users.noreply.github.com>
2021-11-04 09:20:35 +0100

    Create usuarioagrupo.ps1

---
+++ usuario/usuarioagrupo.ps1
@@ -0,0 +1,14 @@
+$dominio= "castellon.upv"
+$suf= "es"
+$domainComponent="dc="+$dominio+",dc="+$suf
+
+{
+  Import-Module ActiveDirectory
+}
+
+$fileUsersCsv=Read-Host "Introduce el fichero csv de los usuarios:"
+$fichero = import-csv -Path $fileUsersCsv -Delimiter :
+
+foreach($linea in $fichero)
+{
+	Add-ADGroupMember -Identity $linea.Group -Members $linea.Name
