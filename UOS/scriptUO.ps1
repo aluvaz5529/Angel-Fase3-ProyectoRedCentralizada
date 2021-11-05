44ddb2cd3a79bc197431d461ae05fed560e183fa
aluvaz5529 <91330510+aluvaz5529@users.noreply.github.com>
2021-11-05 12:55:21 +0100

    Update scriptUO.ps1

--- UOS/scriptUO.ps1
+++ UOS/scriptUO.ps1
@@ -2,5 +2,6 @@ $ficheroCsvUO=Read-Host "Introduce el fichero csv de UO's"
 $fichero = import-csv -Path $ficheroCsvUO -delimiter :
 foreach($line in $fichero)
 {
-   New-ADOrganizationalUnit -Description:$line.Description -Name:$line.Name -Path:$line.Path -ProtectedFromAccidentalDeletion:$true
+   New-ADOrganizationalUnit -Description:$line.Description -Name:$line.Name -Path:$line.Path -ProtectedFromAccidentalDeletion:$false
 }
+#el ultimo parametro es lo que te evita que lo borres accidentalmente asi que si lo pones en false para hacer pruebas es mucho mas comodo y cuando tengas claro que esta bien pones true
