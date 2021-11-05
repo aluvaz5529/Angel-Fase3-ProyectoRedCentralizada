2f4386381afe86d42e28d976f452e59477e06c72
aluvaz5529 <91330510+aluvaz5529@users.noreply.github.com>
2021-11-05 16:52:39 +0100

    Update scriptequipos.ps1

--- equipos/scriptequipos.ps1
+++ equipos/scriptequipos.ps1
@@ -1,7 +1,6 @@
-param($dominio,$sufijoDominio)
-$domainComponent="dc="+$dominio+",dc="+$sufijoDominio
 $equiposCsv=Read-Host "Introduce el fichero csv de Equipos:"
 $fichero= import-csv -Path $equiposCsv -delimiter ":"
 foreach($line in $fichero)
-$pathObject=$line.Path+","+$domainComponent
+	{
 New-ADComputer -Enabled:$true -Name:$line.Computer -Path:$pathObject -SamAccountName:$line.Computer
+	}
