465cb486845d95df56ed26640bd607e75897ce7d
aluvaz5529 <91330510+aluvaz5529@users.noreply.github.com>
2021-11-05 15:29:50 +0100

    Update scriptgrupos.ps1

--- grupos/scriptgrupos.ps1
+++ grupos/scriptgrupos.ps1
@@ -1,25 +1,7 @@
-$gruposCsv=Read-Host "Introduce el fichero csv de Grupos"
+$gruposCsv=Read-Host "Introduce el fichero csv de Grupos:"
 $fichero = import-csv -Path $gruposCsv -delimiter :
 foreach($linea in $fichero)
+
 {
-$pathObject=$linea.Path+","+$domainComponent
-
-New-ADGroup -Name:$linea.Name -Description:$linea.Description `
--GroupCategory:$linea.Category `
--GroupScope:$linea.Scope  `
--Path:$pathObject
-
+	New-ADGroup -Name:$linea.Name -Description:$linea.Description -GroupCategory:$linea.Category -GroupScope:$linea.Scope -Path:$linea.Path
 }
-
-
-#VERSION 2
-$gruposCsv=Read-Host "Introduce el fichero csv de Grupos"
-$fichero = import-csv -Path $gruposCsv -delimiter :
-foreach($linea in $fichero)
-{
-	$pathObject=$linea.Path+","+$domainComponent
-  New-ADGroup -Name:$linea.Name -Description:$linea.Description `
-		-GroupCategory:$linea.Category `
-		-GroupScope:$linea.Scope  `
-		-Path:$pathObject
-	}
